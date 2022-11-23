cc587fe5d084e7e452966a69c41453a1716c0579
meliza <201913306k@utea.edu.pe>
2022-11-22 21:21:11 -0500

    actualizacion

---
+++ agua.php
@@ -0,0 +1,27 @@
+<?php
+
+//======================================================================
+// Clase para Agua
+//======================================================================
+class Agua
+{
+    //-----------------------------------------------------
+    // GET
+    //-----------------------------------------------------
+
+    /**
+     * Método que te indica el estado del agua
+     * @param {float} $temperatura - Temperatura
+     * @return {string} - 'Sólido', 'Líquido' o 'Gaseoso'. Devuelve NULL si tiene un argumento inválido.
+     */
+    function getEstado(float $temperatura): string
+    {
+        if (is_float($temperatura) || is_int($temperatura)) {
+            if ($temperatura <= 0) return 'Sólido';
+            if (0 < $temperatura && $temperatura < 100) return 'Líquido';
+            if (100 <= $temperatura) return 'Gaseoso';
+        } else {
+            return NULL;
+        }
+    }
+}
